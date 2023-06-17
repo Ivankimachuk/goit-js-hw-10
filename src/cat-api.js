86fd60f31677f21852940b759aa4d8ca8e39a286
Ivankimachuk <123023827+Ivankimachuk@users.noreply.github.com>
2023-06-17 22:05:32 +0300

    test

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,35 +1,35 @@
-
-export const API_KEY = "live_M6UV2vFSAMJ9QGBGWKswSAzMFpr3cDC8Ddf5qYHPsCiUUHK1qSZiljL0xy19uYhC";
+const API_KEY = 'live_M6UV2vFSAMJ9QGBGWKswSAzMFpr3cDC8Ddf5qYHPsCiUUHK1qSZiljL0xy19uYhC';
 
 export function fetchBreeds() {
-  const url = "https://api.thecatapi.com/v1/breeds";
+  const url = 'https://api.thecatapi.com/v1/breeds';
   return fetch(url, {
     headers: {
       "x-api-key": API_KEY
     }
   })
-    .then((response) => response.json())
-    .then((data) => {
-      return data;
-    })
-    .catch((error) => {
-      console.error(error);
-      throw error;
-    });
+  .then((response) => response.json())
+  .then((data) => {
+    return data;
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 }
 
+
+
 export function fetchCatByBreed(breedId) {
-  const url = `https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`;
-  return fetch(url, {
+  const urlId = `https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`;
+  return fetch(urlId, {
     headers: {
       "x-api-key": API_KEY
     }
   })
-    .then((response) => response.json())
-    .then((data) => data[0])
-    .catch((error) => {
-      console.error(error);
-      throw error;
-    });
+  .then((response) => response.json())
+  .then((data) => data[0])
+  .catch((error) => {
+    console.log(error)
+  })
+  
 }
 
--- src/index.js
+++ src/index.js
@@ -1,19 +1,22 @@
-import { fetchBreeds, fetchCatByBreed } from './cat-api';
-
-
+import {fetchBreeds, fetchCatByBreed} from './cat-api';
+import SlimSelect from 'slim-select'
 
 const breedSelect = document.querySelector('.breed-select');
 const loader = document.querySelector('.loader');
 const error = document.querySelector('.error');
 const catInfo = document.querySelector('.cat-info');
 
-function populateBreedSelect(breeds) {
-  breeds.forEach((breed) => {
-    const option = document.createElement('option');
-    option.value = breed.id;
-    option.textContent = breed.name;
-    breedSelect.appendChild(option);
-  });
+// new SlimSelect({
+//   select: breedSelect
+// })
+
+function idCatBreedSelect(breeds) {
+  const optionsHTML = breeds
+  .map((bread) => ` <select id="single">
+                      <option value='${bread.id}'>${bread.name}</option>
+                    // </<select>` ) 
+  .join('');
+  breedSelect.innerHTML = optionsHTML;
 }
 
 function showLoader() {
@@ -27,62 +30,51 @@ function hideLoader() {
 function showError() {
   error.style.display = 'block';
 }
-
 function hideError() {
   error.style.display = 'none';
 }
 
 function showCatInfo(cat) {
-    if (cat.length > 0) {
-      const image = document.createElement('img');
-      image.src = cat[0].url;
-  
-      const name = document.createElement('p');
-      name.textContent = cat[0].breeds[0].name;
-  
-      const description = document.createElement('p');
-      description.textContent = cat[0].breeds[0].description;
-  
-      const temperament = document.createElement('p');
-      temperament.textContent = cat[0].breeds[0].temperament;
-  
-      catInfo.innerHTML = '';
-      catInfo.appendChild(image);
-      catInfo.appendChild(name);
-      catInfo.appendChild(description);
-      catInfo.appendChild(temperament);
-    } else {
-      showError();
-    }
+  if(cat.breeds.length > 0) {
+    const infoHTML = `
+          <img  src="${cat.url}" class="cat-image" width="700" height="400" />
+          <p class="cat-text">${cat.breeds[0].name}</p>
+          <p class='cat-info-text'>${cat.breeds[0].description}</p>
+          <p class='cat-info-text'>${cat.breeds[0].temperament}</p>
+    `;
+    catInfo.innerHTML = infoHTML;
+  }else {
+    showError();
   }
+}
+
 function onBreedSelectChange() {
   const breedId = breedSelect.value;
   showLoader();
   hideError();
 
   fetchCatByBreed(breedId)
-    .then((cat) => {
-      hideLoader();
-      showCatInfo(cat);
-    })
-    .catch((error) => {
-      hideLoader();
-      showError();
-    });
-}
-
-breedSelect.addEventListener('change', onBreedSelectChange);
-
-showLoader();
-hideError();
-
-fetchBreeds()
-  .then((breeds) => {
+  .then((cat) => {
     hideLoader();
-    populateBreedSelect(breeds);
+    showCatInfo(cat)
   })
   .catch((error) => {
     hideLoader();
     showError();
   });
+}
 
+breedSelect.addEventListener('change' , onBreedSelectChange);
+
+showLoader();
+hideError();
+
+fetchBreeds()
+.then((breeds) => {
+  hideLoader();
+  idCatBreedSelect(breeds) 
+})
+.catch((error) => {
+  hideLoader();
+  showError();
+})
